bf8f27615ca743ac4cf3dd572b0329f17781daf4
veitm1 <veitm1@AWDT005>
2020-01-16 15:16:28 -0600

    Ongoing Dev.

---
+++ grid.js
@@ -0,0 +1,14 @@
+var gridValues = [
+    ["","0","1","2","3","4","5","6","7","8","9"],
+    ["1","MV","JP","KC","MS","TM","AS","GL","KH","MM","SH"],
+    ["2","SH","MV","JP","KC","MS","TM","AS","GL","KH","MM"],
+    ["3","MM","SH","MV","JP","KC","MS","TM","AS","GL","KH"],
+    ["4","KH","MM","SH","MV","JP","KC","MS","TM","AS","GL"],
+    ["5","GL","KH","MM","SH","MV","JP","KC","MS","TM","AS"],
+    ["6","AS","GL","KH","MM","SH","MV","JP","KC","MS","TM"],
+    ["7","TM","AS","GL","KH","MM","SH","MV","JP","KC","MS"],
+    ["8","MS","TM","AS","GL","KH","MM","SH","MV","JP","KC"],
+    ["9","KC","MS","TM","AS","GL","KH","MM","SH","MV","JP"],
+    ["0","JP","KC","MS","TM","AS","GL","KH","MM","SH","MV"]
+                    
+];
---
+++ site.js
@@ -0,0 +1,32 @@
+var gameId = '2017012200';
+var URL = 'http://www.nfl.com/liveupdate/game-center/' + gameId + '/' + gameId + '_gtd.json';
+var logoUrl = 'https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/';
+var refreshTime = 1000;
+
+$(document).ready(function() { 
+    getScores(); 
+});
+
+function getScores(){
+    $.ajax({
+        url: URL, success: function (data) {
+            gameData = data[gameId];
+            console.log(gameData);
+            $("#homeTeam").html( gameData.home.abbr);
+            $("#homeLogo").html(`<img src="${logoUrl + gameData.home.abbr}.svg" alt="Home Team" class="teamLogo"></img>`);
+            $("#homeScore").html( gameData.home.score.T);
+            $("#awayTeam").html( gameData.away.abbr);
+            $("#awayLogo").html(`<img src="${logoUrl + gameData.away.abbr}.svg" alt="Away Team" class="teamLogo"></img>`);
+            $("#awayScore").html( gameData.away.score.T);
+            $("#qtr").html( gameData.qtr);
+            $("#down").html( gameData.down);
+            $("#togo").html( gameData.togo);
+            $("#posTeam").html( gameData.posTeam);
+            $("#yl").html( gameData.yl);
+            $("#clock").html( gameData.clock);
+
+            refreshTime = gameData.nextupdate * 1000;
+        }
+    });
+    //setTimeout(refreshTime,getScores());
+}
